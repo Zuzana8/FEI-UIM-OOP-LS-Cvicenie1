552e17482d574e4ec20da5229296563d69d1d45b
Zuzana8 <xkittnerova@stuba.sk>
2022-03-01 00:40:28 +0100

    premenne

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -2,6 +2,22 @@ package sk.stuba.fei.uim.oop;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int celeCislo = 5;
+        long celeVacsieCislo = 5L;
+        float desatinneCislo = 5.5f;
+        double dlhsieDesatinneCislo = 5.5f;
+        char znak = 'a';
+        String retazec = "retazec";
+        boolean bool = false;
+
+        int podiel = 2/3; //0
+        System.out.println(podiel);
+
+        int zvysok = 2%3; //2
+        System.out.println(zvysok);
+
+        float podielFloat = 2/3.0f; //0
+        System.out.println(podielFloat);
+
     }
 }
